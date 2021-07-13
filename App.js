a510eb5d56b9ff6b084aaf8abd03ed4cd1799a64
eLIBERP <>
2021-07-13 22:08:30 +0800

    removed irrelevant codes, added imports

--- App.js
+++ App.js
@@ -2,24 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import home from './screens/home'
+import home from './screens/home';
+import login from './screens/login';
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="login" component={login} />
         <Stack.Screen name="home" component={home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
